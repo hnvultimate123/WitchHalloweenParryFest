a1034dd8b75bed2d316f276f5c167d476b42ed4d
httrieu <103277743+httrieu@users.noreply.github.com>
2024-10-09 20:40:29 -0700

    not working

--- Game/Assets/Scripts/Parry.cs
+++ Game/Assets/Scripts/Parry.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Parry : MonoBehaviour
 {
+
+    public GameObject objectToActivateAndDeactivate;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        objectToActivateAndDeactivate.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //objectToActivateAndDeactivate.SetActive(false);
+        if (Input.GetKeyDown("space"))
+        {
+            objectToActivateAndDeactivate.SetActive(true);
+        }
     }
 }
