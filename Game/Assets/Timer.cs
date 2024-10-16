32fcf809f198b8067a36deb790be5473ef17702c
vdvo1029 <vincentvo1029@gmail.com>
2024-10-15 17:30:15 -0700

    timer

---
+++ Game/Assets/Timer.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+
+    [SerializeField] TextMeshProUGUI timerText;
+    float elapsedTime;
+    // Update is called once per frame
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+        timerText.text = elapsedTime.ToString();
+    }
+}
