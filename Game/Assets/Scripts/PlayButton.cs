88c9ef2ffc81cebbada1bda185a4861ec7648598
ValNguyen <van229@sdsc.edu>
2024-10-15 18:02:20 -0700

    play button

---
+++ Game/Assets/Scripts/PlayButton.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayButton : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync("Prototype 2");
+    }
+}
