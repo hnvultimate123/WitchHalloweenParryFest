28ec1c3058f3e80f386c0b2e0b3d60c448c2ac54
hnvultimate123 <43553297+hnvultimate123@users.noreply.github.com>
2024-10-15 19:07:28 -0700

    collision detection!

--- Game/Assets/Scripts/PlayerCollision.cs
+++ Game/Assets/Scripts/PlayerCollision.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour
 {
+    Rigidbody2D m_Rigidbody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_Rigidbody = GetComponent<Rigidbody2D>();
+    
     }
 
     // Update is called once per frame
@@ -15,4 +18,11 @@ public class PlayerCollision : MonoBehaviour
     {
         
     }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Ouput the Collision to the console
+        Debug.Log("Collision : " + collision.gameObject.name);
+    }
+
 }
