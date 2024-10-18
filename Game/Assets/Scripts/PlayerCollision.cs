21b3f86bd86eb86c88520330a0263e5bd7d4a5bf
Rena Hu <r3hu@ucsd.edu>
2024-10-17 18:23:49 -0700

    added collision sound

--- Game/Assets/Scripts/PlayerCollision.cs
+++ Game/Assets/Scripts/PlayerCollision.cs
@@ -6,12 +6,19 @@ public class PlayerCollision : MonoBehaviour
 {
     Rigidbody2D m_Rigidbody;
     public GameObject button;
+    public AudioSource audioSource;
+    public AudioClip collisionSound;
 
     // Start is called before the first frame update
     void Start()
     {
         button.SetActive(false);
         m_Rigidbody = GetComponent<Rigidbody2D>();
+        if (audioSource == null)
+        {
+            audioSource = GetComponent<AudioSource>();
+        }
+            
     }
 
     // Update is called once per frame
@@ -24,6 +31,11 @@ public class PlayerCollision : MonoBehaviour
     {
         //Ouput the Collision to the console
         if(collision.gameObject.tag == "Bullet") {
+            
+            if (audioSource != null && collisionSound != null)
+            {
+                audioSource.PlayOneShot(collisionSound);
+            }
             Debug.Log("Collision : " + collision.gameObject.name);
             PauseGame();
             button.SetActive(true);
